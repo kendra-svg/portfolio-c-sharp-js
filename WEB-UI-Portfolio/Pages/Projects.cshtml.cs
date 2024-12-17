4f608be6a3abd49832e4360dcdc5c9cbac0a821a
kendra-svg <ksanchov@ucenfotec.ac.cr>
2024-12-17 12:23:05 -0600

    Add correct project files

---
+++ WEB-UI-Portfolio/Pages/Projects.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WEB_UI_Portfolio.Pages
+{
+    public class ProjectsModel : PageModel
+    {
+        private readonly ILogger<ProjectsModel> _logger;
+
+        public ProjectsModel(ILogger<ProjectsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
