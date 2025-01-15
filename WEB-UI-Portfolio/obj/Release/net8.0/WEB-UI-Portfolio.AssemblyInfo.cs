372347e1e95291b6828b6dd38bfe014cb09f8b7b
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-01-15 08:01:48 -0600

    Add color to navbar and misc files

--- WEB-UI-Portfolio/obj/Release/net8.0/WEB-UI-Portfolio.AssemblyInfo.cs
+++ WEB-UI-Portfolio/obj/Release/net8.0/WEB-UI-Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEB-UI-Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595e5e5154d05b29e43361cb4178f3c2db2d87ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f650863ef951e45cb8d425aec63fe3a2a85324fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEB-UI-Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEB-UI-Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
